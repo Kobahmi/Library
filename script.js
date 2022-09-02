99c82f706b873de3ca90d1c73299796b5d8ec830
Nobel <nobel_lamun@hotmail.com>
2022-09-01 23:11:50 -0230

    fixed library array

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-let myLibrary = [
-    {
-        title: "test",
-        author: "tester",
-        pages: "235",
-        read: false
-    }
-];
+let myLibrary = [];
 
 
 class Book {
@@ -18,12 +11,12 @@ class Book {
 }
 
 
-const formBox = document.querySelector("#add-book-form")
+const formBox = document.getElementById("add-book-form")
 const submitInput = formBox.querySelector("#submitBtn")
-const titleInput = formBox.querySelector("#title")
-const authorInput = formBox.querySelector("#author")
-const pagesInput = formBox.querySelector("#pages")
-const readInput = formBox.querySelector("#read")
+const titleInput = formBox.elements["title"]
+const authorInput = formBox.elements["author"]
+const pagesInput = formBox.elements["pages"]
+const readInput = formBox.elements["read"]
 const addBtn = document.getElementById("addBtn")
 const overlayBox = document.querySelector(".overlay-box")
 const overlayScreen = document.querySelector(".overlay-screen")
@@ -32,16 +25,17 @@ const bookContainer = document.querySelector("#book-container")
 
 addBtn.addEventListener("click", activate)
 overlayScreen.onclick = removeActivate
-formBox.addEventListener("submit", () => {
-    let newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
+formBox.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let newBook = new Book(title.value, author.value, pages.value, read.checked);
     addBookToLibrary(newBook)
     removeActivate()
 })
 
 
 function addBookToLibrary(book) {
-    myLibrary.push(book)
-    createBookCard()
+    myLibrary.push(book);
+    createBookCard();
 }
 
 function activate() {
@@ -64,6 +58,8 @@ function resetInput() {
 
 
 function createBookCard() {
+    let newBook = myLibrary[myLibrary.length - 1];
+
     const bookCard = document.createElement("div")
     const newTitle = document.createElement("p")
     const newAuthor = document.createElement("p")
@@ -85,36 +81,48 @@ function createBookCard() {
     readBtn.classList.add("btn")
     removeBtn.classList.add("btn")
 
-    newTitle.textContent = title.value
-    newAuthor.textContent = author.value
-    newPages.textContent = pages.value
-    removeBtn.textContent = "Remove"
+    newTitle.innerHTML = newBook.title
+    newAuthor.innerHTML = newBook.author
+    newPages.innerHTML = newBook.pages
+    removeBtn.innerHTML = "Remove"
 
-    if (readInput.checked) {
+    if (newBook.read === true) {
     readBtn.classList.add("btn-read")
-    readBtn.textContent = "Read"
+    readBtn.innerHTML = "Read"
     } else {
     readBtn.classList.add("btn-not-read")
-    readBtn.textContent = "Not read"
+    readBtn.innerHTML = "Not read"
     }
 
     readBtn.addEventListener("click", toggle)
-    removeBtn.addEventListener("click", removeCard)
+    removeBtn.addEventListener("click", removeCard(removeBtn, newBook.title, newBook.author))
 
     function toggle() {
         if (readBtn.classList.contains("btn-read")) {
         readBtn.classList.remove("btn-read")
         readBtn.classList.add("btn-not-read")
-        readBtn.textContent = "Not read"}
+        readBtn.textContent = "Not read"
+        newBook.read = false}
+        
     
         else {
         readBtn.classList.remove("btn-not-read")
         readBtn.classList.add("btn-read")
-        readBtn.textContent = "Read"}
+        readBtn.textContent = "Read"
+        newBook.read = true}
     }
+}
 
-    function removeCard() {
-        bookContainer.removeChild(bookCard);
-        myLibrary.splice(bookCard, 1);
-    }
+function removeCard(btn, title, author) {
+    btn.addEventListener('click', () => {
+        let index = 0;
+        myLibrary.forEach((book) => {
+            if (book.title === title && book.author === author) {
+                myLibrary.splice(index, 1);
+            }
+            index++;
+        })
+        let bookToBeRemoved = btn.parentElement.parentElement;
+        bookToBeRemoved.remove();
+    })
 }
